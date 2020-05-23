d88316f27c4296a21a4e4039b4d66c041557f248
harshpatel1692 <harshpatel1692@gmail.com>
2020-05-23 01:03:33 -0500

    Added script block to HTML. Added script.js with If-Else condition.

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,44 @@
+/**
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+**/
+
+/*If-Else conditions*/
+/**
+var firstName = 'Harsh';
+var civilStatus = 'Married1';
+
+if (civilStatus === 'Married'){
+    console.log('The status of', firstName, 'is Married!');
+} else if (civilStatus === 'Single') {
+   console.log('The status of', firstName, 'is not Married!');
+} else {
+    console.log('The status of', firstName, 'is Unknown');
+}
+**/
+
+/*Ternary Operator*/
+/**
+1>2 ? console.log('Value is Greater') : console.log('Value is Smaller');
+var age = 20
+var drink = age >=18 ? 'Age is Legal.' : 'Age is Minor.';
+
+console.log(drink)
+**/
+
+/*Switch Statement*/
+
+
+
+
+
