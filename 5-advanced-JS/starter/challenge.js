466ecc44da13d8c4b57147b0dbcfd8800ed27d01
harshpatel1692 <harshpatel1692@gmail.com>
2020-06-04 13:20:50 -0500

    Challenge Part 2 - keep the game going

--- 5-advanced-JS/starter/challenge.js
+++ 5-advanced-JS/starter/challenge.js
@@ -19,6 +19,7 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 //Using IIFE (Immediately invoked function expression) so when the block is implemented in some other code, the variables doesn't clash or override.
+/*
 (function(){
     function Question(question, answers, correct){
         this.question = question;
@@ -52,4 +53,51 @@ c) correct answer (I would use a number for this)
     var answer = parseInt(prompt('Please select the correct answer.'));
     questions[n].checkAnswer(answer);
 
+})();
+*/
+
+//Challenge-2
+(function(){
+    function Question(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function(){
+        console.log(this.question);
+        for (var i =0; i<this.answers.length; i++){
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+    Question.prototype.checkAnswer = function(ans){
+        if (ans===this.correct){
+            console.log('Correct Answer!')
+        }else{
+            console.log('Wrong Answer!')
+        }
+    }
+    var q1 = new Question('Is Javascript easy to learn?', ['Yes', 'No'], 0);
+    var q2 = new Question('What is my name?', ['John', 'Harsh', 'Meet'], 1);
+    var q3 = new Question('Sky is?', ['Red', 'Green', 'Blue'], 2);
+    var q4 = new Question('Grass is?', ['Green', 'red', 'Blue'], 0);
+
+    var questions = [q1, q2, q3, q4];
+
+
+    function nextQuestion(){
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer));
+            nextQuestion(); //Looped forever but can break on 'exit'!
+        }
+        //nextQuestion(); //Looped forever!
+    }
+    nextQuestion(); //Call it once
 })();
