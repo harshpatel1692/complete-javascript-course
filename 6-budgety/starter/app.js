6ccbb1d945732bfc3b5b99e49f2518cf14877260
harshpatel1692 <harshpatel1692@gmail.com>
2020-07-02 00:09:19 -0500

    Reading UI fields and setting DOM vars for reusability

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -15,19 +15,44 @@ var budgetController = (function(){
 
 var UIController = (function(){
     //some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    }
+    return {
+        getinput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp value
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings
+        }
+    };
 })();
 
 
 var controller = (function (budgetCtrl, UICtrl){
+
+    var DOMstrings = UICtrl.getDOMstrings();
     var ctrlAddItem = function(){
         //To do list
         //1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
         //2. Add the item to the budget controller
         //3. Add item to the UI
         //4. Calculate the budge and display on UI
-        console.log('This is working');
+
+        // console.log('This is working');
+
     };
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
         if (event.keyCode === 13 || event.which === 13){
