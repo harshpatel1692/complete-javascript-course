dc1d750764b4e77fa3e951946afd45a22ab5e6f2
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-07-07 00:21:42 -0500

    Inserting element to array from UI entries

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -10,7 +10,48 @@ var budgetController = (function(){
         }
     }
 */
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var data = {
+        allItems: {
+            exp:[],
+            inc:[]
+        },
+        total: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+
+            //last ID plus one
+            ID = data.allItems[type].length > 0 ? data.allItems[type][data.allItems[type].length-1].id+1 : 0;
+
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income (ID, des, val)
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function(){
+            return console.log(data);
+        }
+    };
 })();
 
 var UIController = (function(){
@@ -50,11 +91,13 @@ var controller = (function (budgetCtrl, UICtrl){
     };
 
     var ctrlAddItem = function(){
+        var input, newItem;
         //To do list
         //1. Get the field input data
-        var input = UICtrl.getinput();
-        console.log(input);
+        input = UICtrl.getinput();
+        // console.log(input);
         //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add item to the UI
         //4. Calculate the budge and display on UI
 
