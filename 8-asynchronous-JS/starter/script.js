c9c70852c0cce4da2c9ea2a4a6e6338cf9fdd5cc
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-07-19 14:52:18 -0500

    E5 - Callback Hell

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -1,5 +1,6 @@
 //Async/Await, AJAX, Promises
 
+/*
 const second = () =>{
     setTimeout(() => {
         console.log('Second - Async');
@@ -14,3 +15,26 @@ const first = () => {
 }
 
 first();
+*/
+
+//Callback Hell - ES5
+function getRecipe(){
+    setTimeout(()=>{
+        const recipeID = [523, 956, 346, 781, 322];
+        console.log(recipeID);
+
+        setTimeout((id)=>{
+            const recipe = {title: 'Fresh tomato pasta', publisher:'John Doe'};
+            console.log(`${id}: ${recipe.title}`);
+
+            setTimeout(publisher => {
+                const recipe2 = {title: 'Italian Pizza', publisher:publisher};
+                console.log(`${id}: ${recipe2.title}`);
+            }, 1500, recipe.publisher);
+
+        }, 1000, recipeID[2]);
+    }, 1500);
+
+}
+getRecipe();
+
