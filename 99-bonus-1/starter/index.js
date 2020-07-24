fa5363798ceca55ff0f40db8d03a4c707429779c
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-07-24 18:22:16 -0500

    NodeJS templating

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -15,19 +15,47 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     let id = url.parse(req.url, true).query.id;
     if (!id) id = 0;
+
     if (pathName === '/') {
         res.writeHead(200, {'Content-type': 'text/html'});
         res.end('Home Page');
 
-    } else if (pathName === '/products') {
+    }
+    else if (pathName === '/products') {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end('Product Page');
+        fs.readFile(`${__dirname}/overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
 
-    } else if (pathName === '/laptop' && id < laptopData.length) {
+
+            fs.readFile(`${__dirname}/card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                res.end(overviewOutput);
+            });
+        });
+
+        //res.end('Product Page');
+
+
+    }
+    else if (pathName === '/laptop' && parseInt(id)<laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.end(`Laptop Page - ${id}`);
+        fs.readFile(`${__dirname}/laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output)
+        });
 
-    } else {
+        // res.end(`Laptop Page - ${id}`);
+
+    }
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {'Content-type':'image/jpg'});
+            res.end(data);
+        });
+    }
+    else {
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('Page not found!');
     }
@@ -38,3 +66,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('listening request')
 });
+
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+}
